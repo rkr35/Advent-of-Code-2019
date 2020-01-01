4898283abe1ff5b5306c0a9a2c5cbfee8f1b728a
rkr35 <rkr35@drexel.edu>
2020-01-01 15:56:48 -0500

    Add part 2 of day 1

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -11,12 +11,18 @@ fn main() {
 
     let mut current_line = String::new();
     let mut fuel_requirement = 0;
+    let fuel = |mass| mass / 3 - 2;
 
     while f.read_line(&mut current_line).expect("unable to read line from input file") != 0 {
-        let mass: u32 = current_line.trim_end().parse().expect("unable to parse mass");
-        let fuel = mass / 3 - 2;
-        fuel_requirement += fuel;
+        let mass: i32 = current_line.trim_end().parse().expect("unable to parse mass");
         current_line.clear();
+
+        let mut f = fuel(mass);
+
+        while f > 0 {
+            fuel_requirement += f;
+            f = fuel(f);
+        }
     }
 
     println!("{}", fuel_requirement);
