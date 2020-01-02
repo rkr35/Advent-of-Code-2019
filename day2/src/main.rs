72cb59ccba2fe3698060c879841d90f59c468fed
rkr35 <rkr35@drexel.edu>
2020-01-01 19:48:04 -0500

    Refactor

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -3,29 +3,6 @@
 
 use std::io::Read;
 
-enum Kind {
-    Add,
-    Multiply,
-}
-
-struct Opcode {
-    kind: Kind,
-    left: usize,
-    right: usize,
-    store: usize,
-}
-
-impl Opcode {
-    fn execute(&self, program: &mut [usize]) {
-        let left = program[self.left];
-        let right  = program[self.right];
-        program[self.store] = match self.kind {
-            Kind::Add => left + right,
-            Kind::Multiply => left * right,
-        };
-    }
-}
-
 fn main() {
     let mut program = String::new();
     
@@ -44,25 +21,33 @@ fn main() {
 
     // "...before running the program, replace position 1 with the value 12 and 
     // replace position 2 with the value 2."
-    *program.get_mut(1).expect("unable to set position 1") = 12;
-    *program.get_mut(2).expect("unable to set position 2") = 2;
+    *program.get_mut(1).expect("position 1 doesn't exist") = 12;
+    *program.get_mut(2).expect("position 2 doesn't exist") = 2;
 
     for i in (0..program.len()).step_by(4) {
-        let opcode = match *program.get(i).expect("expected opcode") {
-            1 => Kind::Add,
-            2 => Kind::Multiply,
-            99 => break,
+        const ADD: usize = 1;
+        const MULTIPLY: usize = 2;
+        const STOP: usize = 99;
+
+        let opcode = *program.get(i).expect("expected opcode");
+        
+        let left = program.get(i+1).expect("expected left operand");
+        let left = *program.get(*left).expect("left position doesn't exist");
+
+        let right = program.get(i+2).expect("expected left operand");
+        let right = *program.get(*right).expect("right position doesn't exist");
+
+        let store = *program.get(i+3).expect("expected store operand");
+        let store = program
+            .get_mut(store)
+            .expect("store position doesn't exist");
+
+        match opcode {
+            ADD => *store = left + right,
+            MULTIPLY => *store = left * right,
+            STOP => break,
             unrecognized => panic!("unrecognized opcode: \"{}\"", unrecognized),
         };
-        
-        let opcode = Opcode {
-            kind: opcode,
-            left: *program.get(i+1).expect("expected left operand"),
-            right: *program.get(i+2).expect("expected left operand"),
-            store: *program.get(i+3).expect("expected store operand"),
-        };
-
-        opcode.execute(&mut program);
     }
 
 
